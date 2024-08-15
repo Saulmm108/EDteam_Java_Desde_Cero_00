53ea61b815ff0489206f2d4c41b0e9a62ba91461
Saulmm108 <saul.machicado.acad@gmail.com>
2024-08-15 11:52:48 -0400

    Añadimos el archivo Sentencia continue break exit,java

---
+++ Java_Desde_Cero_00/Estructuras_de_Control_Repetitivas_02/Sentencia_Continue_Break_Exit_03.java
@@ -0,0 +1,13 @@
+package Java_Desde_Cero_00.Estructuras_de_Control_Repetitivas_02;
+
+public class Sentencia_Continue_Break_Exit_03 {
+    public static void main(String[] args) {
+        for(int i = 0; i <10; i++){
+            if (i==5){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+    
+}
